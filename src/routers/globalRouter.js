8e6aee77d480472aebeb7c140651bd15ea6e77d7
MinhaLee94 <67772844+shjune10@users.noreply.github.com>
2021-06-03 00:18:40 -0400

    connecting forms with fake DB

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,11 @@
 import express from "express";
 import { join, login } from "../controlers/userController";
-import { search, trending } from "../controlers/videoController";
+import { trending } from "../controlers/videoController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 
 export default globalRouter;
