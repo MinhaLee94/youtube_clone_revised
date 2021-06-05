2a2df9334e148436565bff71f1f231bf6202e413
MinhaLee94 <67772844+shjune10@users.noreply.github.com>
2021-06-05 00:37:43 -0400

    Separating models into init

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { join, login } from "../controlers/userController";
-import { trending } from "../controlers/videoController";
+import { join, login } from "../controllers/userController";
+import { home } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
-globalRouter.get("/", trending);
+globalRouter.get("/", home);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 
